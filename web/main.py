93ac2bc1bbe007097251a62b67d9a7e1be7af437
Vladimir Kocheryzhkin <vladimir.kocheryzhkin@xceleration.com>
2025-09-25 10:04:24 +0300

    migrate to celery 5

--- web/main.py
+++ web/main.py
@@ -9,7 +9,7 @@ from fastapi.openapi.utils import generate_operation_id
 from fastapi.routing import APIRoute
 from pathlib import Path
 
-# from worker import create_task, terminate_task
+from worker import create_task, terminate_task
 
 app = FastAPI()
 
@@ -24,3 +24,52 @@ app = FastAPI()
 })
 def health():
     return JSONResponse({}, status_code=200)
+
+@app.post("/task", status_code=201, responses={
+    201: {
+        "content": {
+            "application/json": {
+                "example": {"id": "08234f72-29c9-4527-861c-b3d29aabf0e4"}
+            }},
+        "description": "Return task id"
+    },
+})
+def submit_task(
+    xslx_file: UploadFile = File(..., description="*.xlsx file with data"),
+):
+    file_location = f"./tmp/{xslx_file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(xslx_file.file.read())
+
+    task = create_task.delay(file_location)
+
+    return JSONResponse({"id": task.id, "status": task.status}, status_code=201)
+
+@app.get("/task/{id}/status", responses={
+    200: {
+        "content": {
+            "application/json": {
+                "example": {
+                    "id": "cc6b3345-4207-4ebc-94a2-0c8f03d08bb3",
+                    "status": "FAILURE"
+                    }
+            }},
+        "description": "Return task id and current status"
+    },
+    404: {
+        "description": "Task with provided id not found"
+    }
+})
+def get_task_status(id):
+    try:
+        task_result = AsyncResult(id)
+
+        result = {
+            "id": id,
+            "status": task_result.status,
+            "result": task_result.result
+        }
+        return JSONResponse(result)
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=404, content={"message": "Not Found"})
--- web/worker.py
+++ web/worker.py
@@ -4,17 +4,22 @@ import json
 
 import pandas as pd
 from pathlib import Path
+import logging
 
 from celery import Celery
 from celery import current_task
-
+import time
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+logger = logging.getLogger('celery.task')
 
 @celery.task(name="create_task")
-def create_task():
+def create_task(path):
+    logger.info("Executing create_task with id: %s and path %s", create_task.request.id, path)
+
+    time.sleep(60) #sec
     return True
 
 @celery.task(name="terminate_task")
