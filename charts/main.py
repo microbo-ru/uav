8096c4606f615f1d31ead04e8b468b06c95f5fa1
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2025-10-02 23:31:54 +0300

    trigger job from the charts

--- charts/main.py
+++ charts/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+import time
 
 st.set_page_config(
     page_title="Charts",
@@ -6,12 +8,54 @@ st.set_page_config(
     layout="wide"
 )
 
-st.sidebar.success("Start analysis by selecting report above.")
+base_url ='http://app-a:8000/task'
 
-st.write("# Welcome to Charts 📈")
+def get_status(job_id):
+    url = f"{base_url}/{job_id}/status"
+    response = requests.get(url)
+    return response.json()
 
-st.markdown(
-    """
-    **👈 Select a report from the left** to see some nice visualizations
-"""
-)
+def submit_job(filename):
+    response = requests.post(base_url)
+
+    headers = {
+        'accept': 'application/json',
+    }
+
+    files = {
+        'xslx_file': (filename, open(f'tmp/{filename}', 'rb'), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
+    }
+
+    response = requests.post(base_url, headers=headers, files=files)
+    
+    st.write(response.json(), response.status_code)
+    return response.json()
+
+xlsx_file = st.sidebar.file_uploader("Загрузите .xlsx файл")
+if xlsx_file is not None:
+    filename = xlsx_file.name
+    file_path = f"tmp/{filename}"
+
+    if st.button("Построить график"):
+        st.write("Нормализация данных...")
+
+        with st.spinner("Ожидаем ответка от API..."):
+            try:
+                with open(file_path, "wb") as f:
+                    f.write(xlsx_file.getbuffer())
+
+                response = submit_job(filename)
+                job_id = response["id"]
+                job_status = response["status"]
+
+                st.write(job_id)
+
+                while (job_status != "SUCCESS"):
+                    response = get_status(job_id)
+                    job_status = response["status"]
+                    st.json(response)
+                    time.sleep(1)
+
+                st.success("Data fetched successfully!")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Error fetching data: {e}")
